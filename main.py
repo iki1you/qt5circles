6fad77e1b6cce5ada84cbd93d69eb3dd900aa00b
iki1you <vovastr0v@yandex.ru>
2020-11-20 17:56:30 +0500

    1

---
+++ main.py
@@ -0,0 +1,80 @@
+import sys
+from random import randint
+
+from PyQt5 import uic
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPainter, QColor, QPen, QBrush
+from PyQt5.QtCore import Qt
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(437, 461)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 437, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "НАЖМИ"))
+
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        uic.loadUi('UI.ui', self)
+        self.setFixedSize(740, 700)
+        self.setWindowTitle('Минипланировщик')
+        self.pushButton.clicked.connect(self.runAction)
+        self.obj = []
+
+    def runAction(self):
+        self.obj.append(Circle(randint(70, 340), randint(70, 300),
+                               randint(70, 340), randint(70, 300)))
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.begin(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        for i in self.obj:
+            i.draw(painter)
+            self.update()
+        painter.end()
+
+
+class Circle:
+    def __init__(self, cx, cy, x, y):
+        self.cx = cx + 200
+        self.cy = cy + 200
+        self.x = x + 200
+        self.y = y + 200
+        self.c = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+
+    def draw(self, painter):
+        painter.setPen(QPen(self.c))
+        painter.setBrush(self.c)
+        radius = int(((self.cx - self.x) ** 2 + (self.cy - self.y) ** 2) ** 0.5)
+        painter.drawEllipse(self.cx - radius, self.cy - radius, 2 * radius, 2 * radius)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = Window()
+    wnd.show()
+    sys.exit(app.exec())
